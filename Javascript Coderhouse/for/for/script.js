11c5f781a3880b2e1d4694a3583c7b6de8688d72
lilcouto <137234106+lilcouto@users.noreply.github.com>
2024-01-12 22:26:10 -0300

    Add files via upload

---
+++ Javascript Coderhouse/for/for/script.js
@@ -0,0 +1,19 @@
+function Tarefa(descricao, prioridade) {
+  this.descricao = descricao;
+  this.prioridade = prioridade;
+  this.concluida = false; 
+  this.concluir = function () {
+    this.concluida = true;
+    console.log(`Tarefa "${this.descricao}" concluída!`);
+  };
+}
+
+
+const primeiraTarefa = new Tarefa("Estudar JavaScript", "Alta");
+
+
+const botaoConcluirTarefa = document.getElementById('botao-concluir');
+
+botaoConcluirTarefa.addEventListener('click', function () {
+  primeiraTarefa.concluir();
+})
