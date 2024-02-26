25e5420325c0c04c3ba25948e6edb23cc8d74840
Pedro Martins <pedrohenrique.251@hotmail.com>
2024-02-26 19:26:33 -0300

    Limpeza de código e adição de links nos produtos.

--- script.js
+++ script.js
@@ -1,53 +1,17 @@
-//Capturar entradas por meio de prompt().
-//Declarar variáveis, objetos e arrays necessários para simular o processo selecionado.
-//Criar funções e/ou métodos para realizar operações (soma, subtração, concatenação, divisão, porcentagem, etc.).
-//Efetuar uma saída, que é o resultado dos dados processados, por alert() ou console.log().
-/*let carrinhoCompras = [];
-
-function adicionarCarrinho() {
-  let nomeItem = prompt("Digite o nome do item:");
-  let quantidadeItem = parseInt(prompt("Digite a quantidade do item:"));
-  let precoItem = parseFloat(prompt("Digite o preço do item:"));
-
-  let novoItem = {
-    nome: nomeItem,
-    quantidade: quantidadeItem,
-    preco: precoItem,
-  };
-
-  carrinhoCompras.push(novoItem);
-
-  console.log(nomeItem + " foi adicionado ao carrinho.");
-}
-
-let adicionarMaisItens = true;
-
-while (adicionarMaisItens) {
-  adicionarCarrinho();
-  adicionarMaisItens = prompt("Deseja adicionar mais um item ao carrinho? (Digite 'sim' ou 'não')").toLowerCase() === 'sim';
-}
-
-let totalCompra = 0;
-
-for (let i = 0; i < carrinhoCompras.length; i++) {
-  totalCompra += carrinhoCompras[i].quantidade * carrinhoCompras[i].preco;
-}
-
-console.log("Conteúdo final do carrinho: ", ...carrinhoCompras);
-console.log("Total do carrinho: " + totalCompra + " reais.");*/
-
 let produtos = [
   {
     id: 1,
     imagem: 'assets/img/buffalo-trace-bourbon-750-ml_1_250.webp',
     nome: 'Whisky Buffalo Trace Bourbon 750 ml',
     preco: 130,
+    pagina: 'projeto_bebidas\whisky_buffalo\whisky.html',
   },
   {
     id: 2,
     imagem: 'assets/img/espumante-cava-freixenet-carta-nevada-semi-seco_1_250.webp',
     nome: 'Espumante Cava Freixenet Carta Nevada 750 ml',
     preco: 60,
+    pagina: 'projeto_bebidas\Espumante Cava Freixenet Carta Nevada 750 ml\espumante.html'
   },
   {
     id: 3,
@@ -102,11 +66,10 @@ let produtos = [
 const categories = [...new Set(produtos.map((item) => { return item }))]
 let i = 0;
 document.getElementById('loja').innerHTML = categories.map((item) => {
-  var { imagem, nome, preco } = item;
+  var { imagem, nome, preco, pagina } = item;
   return (
     `<div class='product'>
-        <img src=${imagem}></img>    
-        <div class='bottom'>
+        <a href=${pagina}><img src=${imagem}></a>
         <p>${nome}</p>
         <h2>R$ ${preco},00</h2>` +
     "<button onclick='addtocart(" + (i++) + ")'>Comprar</button>" +
